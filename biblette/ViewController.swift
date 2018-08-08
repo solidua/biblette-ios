c947b7ffcc213883ad8303cb04be881c4b684a56
Alvin <agsolidum@gmail.com>
2018-08-07 21:31:10 -0700

    Initial Commit

---
+++ biblette/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  biblette
+//
+//  Created by Alvin Solidum on 8/7/18.
+//  Copyright © 2018 mochi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
